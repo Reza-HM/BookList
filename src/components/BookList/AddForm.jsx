6d683f61dd4f251e2c713c153540b3ac716353e5
Reza Hamzei <hamzeiamir205@gmail.comls>
2023-07-26 12:07:35 +0330

    add all files

---
+++ src/components/BookList/AddForm.jsx
@@ -0,0 +1,123 @@
+import React, { Component } from "react";
+import Book from "./Book";
+
+export default class AddForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      books: [],
+
+      title: "",
+      author: "",
+      year: "",
+    };
+  }
+
+  titleHandler(event) {
+    this.setState({
+      title: event.target.value,
+    });
+  }
+
+  authorHandler(event) {
+    this.setState({
+      author: event.target.value,
+    });
+  }
+
+  dateHandler(event) {
+    this.setState({
+      year: event.target.value,
+    });
+  }
+
+  submitHandler(event) {
+    event.preventDefault();
+
+    let { title, author, year } = { ...this.state };
+
+    if (title && author && year) {
+      let newBook = {
+        id: this.state.books.length + 1,
+        title,
+        author,
+        year,
+      };
+
+      this.setState({
+        books: [...this.state.books, newBook],
+      });
+      
+      this.setState({
+        title: "",
+        author: "",
+        year: "",
+      });
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <form
+          id="book-form"
+          onSubmit={(event) => this.submitHandler(event)}
+          autoComplete="off"
+        >
+          <div className="form-group">
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              id="title"
+              value={this.state.title}
+              onChange={(event) => this.titleHandler(event)}
+              className="form-control forClearing"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="author">Author</label>
+            <input
+              type="text"
+              id="author"
+              value={this.state.author}
+              onChange={(event) => this.authorHandler(event)}
+              className="form-control forClearing"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="year">Year</label>
+            <input
+              type="text"
+              id="year"
+              value={this.state.year}
+              onChange={(event) => this.dateHandler(event)}
+              className="form-control forClearing"
+            />
+          </div>
+          <input
+            type="submit"
+            value="Add Book"
+            className="btn btn-warning btn-block add-btn"
+          />
+        </form>
+        <table className="table table-striped mt-5 text-center">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Author</th>
+              <th>Year</th>
+            </tr>
+          </thead>
+          <tbody id="book-list">
+            {this.state.books.map((book) => (
+              <Book {...book} key={book.id} />
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
+  }
+}
